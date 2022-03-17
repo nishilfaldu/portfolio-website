8c74c81a3b3713d957e516420db8fa9e6f2aae61
Zenith Zack <faldund@outlook.com>
2022-03-17 00:06:10 -0400

    edited the resume section

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -2,19 +2,19 @@ import React from "react";
 
 const Resume = ({ data }) => {
   if (data) {
-    var skillmessage = data.skillmessage;
-    var education = data.education.map(function (education) {
-      return (
-        <div key={education.school}>
-          <h3>{education.school}</h3>
-          <p className="info">
-            {education.degree} <span>&bull;</span>
-            <em className="date">{education.graduated}</em>
-          </p>
-          <p>{education.description}</p>
-        </div>
-      );
-    });
+    // var skillmessage = data.skillmessage;
+    // var education = data.education.map(function (education) {
+    //   return (
+    //     <div key={education.school}>
+    //       <h3>{education.school}</h3>
+    //       <p className="info">
+    //         {education.degree} <span>&bull;</span>
+    //         <em className="date">{education.graduated}</em>
+    //       </p>
+    //       <p>{education.description}</p>
+    //     </div>
+    //   );
+    // });
     var work = data.work.map(function (work) {
       return (
         <div key={work.company}>
@@ -27,44 +27,44 @@ const Resume = ({ data }) => {
         </div>
       );
     });
-    var skills = data.skills.map(function (skills) {
-      var className = "bar-expand " + skills.name.toLowerCase();
-      return (
-        <li key={skills.name}>
-          <span style={{ width: skills.level }} className={className}></span>
-          <em>{skills.name}</em>
-        </li>
-      );
-    });
+    // var skills = data.skills.map(function (skills) {
+    //   var className = "bar-expand " + skills.name.toLowerCase();
+    //   return (
+    //     <li key={skills.name}>
+    //       <span style={{ width: skills.level }} className={className}></span>
+    //       <em>{skills.name}</em>
+    //     </li>
+    //   );
+    // });
   }
 
   return (
     <section id="resume">
-      <div className="row education">
+      {/* <div className="row education">
         <div className="three columns header-col">
           <h1>
             <span>Education</span>
           </h1>
-        </div>
+        </div> 
 
         <div className="nine columns main-col">
           <div className="row item">
             <div className="twelve columns">{education}</div>
           </div>
         </div>
-      </div>
+      </div> */}
 
       <div className="row work">
         <div className="three columns header-col">
           <h1>
-            <span>Work</span>
+            <span>Experience</span>
           </h1>
         </div>
 
         <div className="nine columns main-col">{work}</div>
       </div>
 
-      <div className="row skill">
+      {/* <div className="row skill">
         <div className="three columns header-col">
           <h1>
             <span>Skills</span>
@@ -78,7 +78,7 @@ const Resume = ({ data }) => {
             <ul className="skills">{skills}</ul>
           </div>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
